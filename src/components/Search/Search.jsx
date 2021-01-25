a0d70a7761988db2d5750e4162738f468b1e0bdd
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-24 23:57:51 -0500

    search component styled and commented

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = (props) => {
+    return (
+      //   filtering employees through searchbox 
+      <div className="create-form" style={{textAlign: "center"}}>
+        <div className="input-group">
