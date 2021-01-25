a08fddb5f125f3a82277380cbdc5f9fe81925e8c
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-24 23:50:42 -0500

    adjusted code

--- src/components/Empolyee/Employee.jsx
+++ src/components/Empolyee/Employee.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-// employee information that will be displayed in a row on the table
+
+// employee information thats rendered in a row on the table
 const EmployeeRow = (props) => {
   return (
     <tr>
       <th scope="row">
-        <img alt="Employee Head shot" src={props.picture.thumbnail} />
+        <img alt="photo of employee" src={props.picture.thumbnail} />
       </th>
       <td>
         {props.name.first} {props.name.last}
