8ab207282f87cbd65e3b11454c17fdb088d0c695
samiandreas70@gmail.com <samiandreas70@gmail.com>
2021-01-24 23:59:42 -0500

    complete search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,3 +5,16 @@ const Search = (props) => {
       //   filtering employees through searchbox 
       <div className="create-form" style={{textAlign: "center"}}>
         <div className="input-group">
+        <input
+          type="text"
+          className="form-control mb-3 mt-3"
+          placeholder="Search Employee"
+          aria-label="Search Employee"
+          onChange={props.onChange}
+        ></input>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
